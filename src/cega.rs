8cb62fd8747b2d3a69b243615beaa652215d614d
Kenzi Connor <knzconnor@gmail.com>
2024-06-16 16:00:23 -0700

    a little closer to generic

--- src/cega.rs
+++ src/cega.rs
@@ -6,12 +6,19 @@ pub mod binary {
     //     }
 
     pub fn to_bits(byte: u8, len: u8) -> Vec<u8> {
-        (0..8 / len).rev().map(|n| (byte >> n) & 1).collect()
+        match len {
+            1 | 2 | 4 => (0..8 / len)
+                .rev()
+                .map(|n| (byte >> (n * len)) & len)
+                .collect(),
+            _ => panic!("invalid bit length"),
+        }
     }
 
     #[test]
     fn to_bits_handles_u8() {
         let byte: u8 = 0b00011011;
         assert_eq!(to_bits(byte, 1), [0, 0, 0, 1, 1, 0, 1, 1]);
+        //assert_eq!(to_bits(byte, 2), [0, 1, 2, 3]);
     }
 }
