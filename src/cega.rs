77d85e5e1e4bc3b13cf70b28c95faca39e889e61
Kenzi Connor <knzconnor@gmail.com>
2024-06-16 18:30:46 -0700

    handle word size 1,2,4

--- src/cega.rs
+++ src/cega.rs
@@ -5,20 +5,22 @@ pub mod binary {
     //         (0..8 / len).rev().map(|n| (byte >> n) & 1).collect()
     //     }
 
-    pub fn to_bits(byte: u8, len: u8) -> Vec<u8> {
+    pub fn bit_vec(byte: u8, len: u8) -> Vec<u8> {
+        let mask: u8 = 2_u8.pow(len.into()) - 1;
         match len {
             1 | 2 | 4 => (0..8 / len)
                 .rev()
-                .map(|n| (byte >> (n * len)) & len)
+                .map(|n| byte >> (n * len) & mask)
                 .collect(),
-            _ => panic!("invalid bit length"),
+            _ => panic!("invalid word length"),
         }
     }
 
     #[test]
-    fn to_bits_handles_u8() {
+    fn bit_vec_handles_u8() {
         let byte: u8 = 0b00011011;
-        assert_eq!(to_bits(byte, 1), [0, 0, 0, 1, 1, 0, 1, 1]);
-        //assert_eq!(to_bits(byte, 2), [0, 1, 2, 3]);
+        assert_eq!(bit_vec(byte, 1), [0, 0, 0, 1, 1, 0, 1, 1]);
+        assert_eq!(bit_vec(byte, 2), [0b00, 0b01, 0b10, 0b11]);
+        assert_eq!(bit_vec(byte, 4), [0b0001, 0b1011]);
     }
 }
